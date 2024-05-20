845a083b398727dd5687749f51e0f208aa8cff34
Lukas Neukom <6952622+lukasneukom@users.noreply.github.com>
2024-05-20 12:02:52 -0700

    Add swift-markdown dependency to generator

--- generator/Package.swift
+++ generator/Package.swift
@@ -5,10 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "generator",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-markdown.git", branch: "main"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "generator"),
+            name: "generator",
+            dependencies: [
+                .product(name: "Markdown", package: "swift-markdown")
+            ]
+        ),
     ]
 )
--- generator/Sources/main.swift
+++ generator/Sources/main.swift
@@ -1,4 +1,15 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import Markdown
 
-print("Hello, world!")
+let source = """
+# Lukas Neukom
+Hallo Internet — Ich heisse Lukas und bin Bauingenieur.
+Aktuell befinde ich mich in einer beruflichen Auszeit.
+Zuvor war ich Projekt- und Bauleiter im Tiegbau bei der [Grob Ingenieure AG](https://grob.me).
+Meine Hobbies sind lesen, schreiben, lernen, spielen und Sport.
+
+Diese Seite ist von Hand mit HTML, CSS und einer Prise [Code Golf](https://code.golf) programmiert.
+"""
+
+let document = Document(parsing: source)
+print(document.debugDescription())
